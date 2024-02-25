21754e934b219b79d83b8f1925e1858efa9e6f77
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-02-25 23:52:07 +0200

    addd

--- vite-project2/catpics.js
+++ vite-project2/catpics.js
@@ -9,129 +9,32 @@ export function showPics(element) {
         const response = await fetch('catpics.json');
         if (!response.ok) throw new Error('Huono haku!!');
         const images = await response.json();
+
+        const randomIndex = Math.floor(Math.random() * images.length);
+        console.log(randomIndex)
   
         console.log(images);
-        const alt = images[1].name;
-        const figurecap = images[1].description;
-        const imagesrc = images[1].address;
+        const alt = images[randomIndex].name;
+        const figurecap = images[randomIndex].description;
+        const imagesrc = images[randomIndex].address;
   
         // haetaan kuvaelementti html tiedostosta
-        const kuva = document.querySelector('img');
+        const kuva = document.querySelector('figure img');
         kuva.src = imagesrc;
         kuva.alt = alt;
   
         // haetaan kuvateksti html tiedostosta
-        const kuvateksti = document.querySelector('figcaption');
+        const kuvateksti = document.querySelector('figure figcaption');
         kuvateksti.innerText = figurecap;
       } catch (error) {
         console.log(error);
       }
     }
-  
-    // versio 2. jossa luodaan dom elementit itse
-    async function createPics() {
-      console.log('Luodaan kuvat lennossa!');
-  
-      try {
-        // fetch
-        const response = await fetch('catpics.json');
-        if (!response.ok) throw new Error('Huono haku!!');
-        const images = await response.json();
-  
-        console.log(images);
-  
-        // loopataan kaikki kissakuvat läpi ja generoidaan
-        // jokaisesta figure, img ja figcaption elementit
-        images.forEach((item) => {
-          console.log(`Nimi: ${item.name}`);
-        });
-  
-        // luodaan yksi figure
-        // haetaan ensin elementti jojhon haluat että kuvat lisätään
-        const cards = document.querySelector('#cards');
-        cards.innerHTML = '';
-  
-        const alt = images[1].name;
-        const figurecap = images[1].description;
-        const imagesrc = images[1].address;
-  
-        // luo uusi figure elementti
-        const figure = document.createElement('figure');
-        cards.appendChild(figure);
-  
-        // luodaan img elementti
-        const image = document.createElement('img');
-        image.src = imagesrc;
-        image.alt = alt;
-  
-        figure.appendChild(image);
-  
-        // luodaan figcaption elementti
-        const figurecaption = document.createElement('figurecaption');
-        // luodaa teksti figcaptionin sisälle
-        const node = document.createTextNode(figurecap);
-        // lisätää se figcaptionin lapseksi
-        figurecaption.appendChild(node);
-  
-        figure.appendChild(figurecaption);
-  
-        // kommentti
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  
-    // versio 3. jossa loopataan kaikki kuvat
-    async function createPicsLoop() {
-      console.log('Luodaan kuvat lennossa!');
-  
-      try {
-        // fetch
-        const response = await fetch('catpics.json');
-        if (!response.ok) throw new Error('Huono haku!!');
-        const images = await response.json();
-  
-        console.log(images);
-        console.log('Tehdään loopissa');
-  
-        // luodaan yksi figure
-        // haetaan ensin elementti jojhon haluat että kuvat lisätään
-        const cards = document.querySelector('#cards');
-        cards.innerHTML = '';
-  
-        // loopataan kaikki kissakuvat läpi ja generoidaan
-        // jokaisesta figure, img ja figcaption elementit
-        images.forEach((item) => {
-          console.log(`Nimi: ${item.name}`);
-  
-          // luo uusi figure elementti
-          const figure = document.createElement('figure');
-          cards.appendChild(figure);
-  
-          // luodaan img elementti
-          const image = document.createElement('img');
-          image.src = item.address;
-          image.alt = item.name;
-  
-          figure.appendChild(image);
-  
-          // luodaan figcaption elementti
-          const figurecaption = document.createElement('figurecaption');
-          // luodaa teksti figcaptionin sisälle
-          const node = document.createTextNode(item.description);
-          // lisätää se figcaptionin lapseksi
-          figurecaption.appendChild(node);
-  
-          figure.appendChild(figurecaption);
-        });
-  
-        // kommentti
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  
     console.log(element);
-    //element.addEventListener('click', () => modifyHTMLPics());
-    element.addEventListener('click', () => createPicsLoop());
-  }
+    if (element) {
+      // Add event listener only if the element exists
+      element.addEventListener('click', () => 
+      modifyHTMLPics());
+    };
+
+}
--- vite-project2/main.js
+++ vite-project2/main.js
@@ -1,27 +1,73 @@
 import './style.css';
-import './tehtavat.css';
-import javascriptLogo from './javascript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.js';
-import { showJoke } from './joke.js';
 import { showPics } from './catpics.js';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import { jokes } from './jokes.js'
 
-// tehtävät 2
-//import { showPics } from './pics.js';
-// tehtävä 4
-//import { showDiary } from './diary.js';
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <h3>TEHTÄVÄ</h3>
+    <h3>1. Fetch API GET request - Vitsi API rajapinnasta</h3>
+    <p class="read-the-docs">
+      Get your Chuck Norris joke of the day!
+    </p>
 
-document.querySelector('#app').innerHTML = 'Moi täällä ollaan';
+      <h1 id="show_joke"></h1>
 
-// haetaan nappula ja tarjotaan showJoke funktiolle
-let element = document.querySelector('.chuck');
-console.log(element);
-showJoke(element);
-showJoke(document.querySelector('.toinen'));
+<button id="chuck">Hae vitsi</button>
+
+<h3>Tehtävä 2 - Kissakuvat</h3>
+
+    <figure>
+      <img src="http://placekitten.com/300/200" alt="Kissakuva" />
+      <figcaption>Tähän tulee kissakuvan tiedot</figcaption>
+    </figure>
+
+    <button src="catpics.js" class="pics">Hae kissakuvat</button>
+
+    <h3>3. Flexboxilla tehty kortti - Staattinen tieto</h3>
+
+<section class="card-area">
+  <!-- yksittäinen kortti -->
+  <div class="card">
+    <!-- vasen puoli jossa kuva -->
+    <div class="card-img" id="trigger">
+      <img src="gym.jpeg" width="136" alt="Gym" />
+    </div>
+
+    <!-- oikea puoli jossa päiväkirjamerkintä-->
+    <div class="card-diary">
+      <!-- tänne myös flexbox rakenne -->
+      <div class="card-img" id="trigger">
+      <img src="diary.jpeg" alt="diary" />
+    </div>
+    </div>
+  </div>
+</section>
+
+  </div>
+`
+
+
+setupCounter(document.querySelector('#counter'));
+
+const getjoke = document.getElementById('chuck'); 
+const showjoke = document.getElementById('show_joke'); 
+jokes(getjoke, showjoke);
 
 showPics(document.querySelector('.pics'));
 
-// tehtävä 2
-//showPics(document.querySelector('.pics'));
 // tehtävä 4
 //showDiary(document.querySelector('.diary-area'));
