01b52adf7b8823f13936b6112bb186901186dcf1
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-03-18 14:13:13 +0200

    more

--- Vite-project3/yksiloprojekti/fetch.js
+++ Vite-project3/yksiloprojekti/fetch.js
@@ -7,20 +7,20 @@
  * @returns {Object} response json data
  */
 const fetchData = async (url, options = {}) => {
-    let jsonData;
-    try {
-      const response = await fetch(url, options);
-      if (!response.ok) {
-        throw new Error(`HTTP ${response.status} - ${response.statusText}`);
-      }
-  
-      jsonData = await response.json();
-    } catch (error) {
-      console.error('fetchData() error', error);
-      jsonData = {};
+  let jsonData;
+  try {
+    const response = await fetch(url, options);
+    if (!response.ok) {
+      throw new Error(`HTTP ${response.status} - ${response.statusText}`);
     }
-    //console.log(jsonData);
-    return jsonData;
-  };
-  
-  export { fetchData };
+
+    jsonData = await response.json();
+  } catch (error) {
+    console.error('fetchData() error', error);
+    jsonData = {};
+  }
+  //console.log(jsonData);
+  return jsonData;
+};
+
+export { fetchData };
