2629068ca1b2a102faf5ec416f237046f7e85192
TiiVii <111729213+TiiVii@users.noreply.github.com>
2024-03-24 17:28:52 +0200

    last

--- Vite-project3/yksiloprojekti/vite.config.js
+++ Vite-project3/yksiloprojekti/vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         home: resolve(__dirname, 'home.html'),
+        entries: resolve(__dirname, 'entries.html'),
       },
     },
   },
